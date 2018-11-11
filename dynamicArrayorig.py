f866a3c7eca305b0dcda2705eab62f9c3cf3428d
Graphs <graphsandtrees05@gmail.com>
2018-11-11 13:53:56 -0500

    finished h/w part 1

---
+++ dynamicArrayorig.py
@@ -0,0 +1,52 @@
+def nothing(length):
+    l = list()
+    for i in range(length):
+        l.append(None)
+    return l
+
+#nothing() allocates a list of none values-written because i couldnt find a default function to do so.
+
+class dynamicArray:
+    def __init__(self, data):
+        self.data = data
+        self.length = len(data)
+
+    def append(self, value):
+        if (self.length) == len(self.data):
+            tempdata = self.data
+            self.data = nothing(2*int(self.length))
+            for i in range(self.length):
+                self.data[i] = int(tempdata[i])
+            self.data[self.length] = value
+            self.length += 1
+        else:
+            self.data[self.length] = value
+            self.length+=1
+
+    def delete_last_element(self):
+        val = self.data[self.length-1]
+        self.data[self.length-1] = None
+        return val
+    def dumbappend(self, value):
+        if (self.length) == len(self.data):
+            tempdata = self.data
+            self.data = nothing(self.length+1)
+            for i in range(self.length):
+                self.data[i] = int(tempdata[i])
+            self.data[self.length] = value
+            self.length += 1
+        else:
+            self.data[self.length] = value
+            self.length+=1
+
+#testing
+
+a = dynamicArray([1,2,3])
+
+a.append(4)
+print a.data
+a.delete_last_element()
+print a.data
+
+
+#testing program
